ba77dd61c3eb1b5dc35d3d9f93e59e8fca088132
Arash Salehkhah <southernwavesofmadness@protonmail.com>
2023-09-27 16:38:09 +0330

    Fix cors.js

--- cors.js
+++ cors.js
@@ -3,6 +3,9 @@ import cors from "cors";
 // Allow a specific address to access the server (replace with the actual addresses)
 const allowedOrigin = [
   "https://mlfp.chill-hub.net",
+  "https://abyss.chill-hub.net",
+  "http://localhost:4173",
+  "http://localhost:4174",
   "http://localhost:5173",
   "http://localhost:5174",
 ];
